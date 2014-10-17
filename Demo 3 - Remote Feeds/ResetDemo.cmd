23688230cf28372fa467a29e348954ebfe211d32
Justin James <digitaldrummerj@gmail.com>
2014-10-17 15:33:20 -0700

    added missing demo reset files for nuget server
    added missing demo reset files for nuget server

--- Demo 3 - Remote Feeds/ResetDemo.cmd
+++ Demo 3 - Remote Feeds/ResetDemo.cmd
@@ -3,4 +3,4 @@ rmdir packages /S /Q
 del Demo3.RemoteFeeds.sln
 del Demo3.RemoteFeeds.v12.suo /A H
 
-xcopy "..\DemoResetFiles\Demo 3 - RemoteFeeds\*" "..\Demo 3 - Remote Feeds" /E /I /H /Y
+xcopy "DemoResetFiles\Demo 3 - RemoteFeeds\*" "..\Demo 3 - Remote Feeds" /E /I /H /Y
